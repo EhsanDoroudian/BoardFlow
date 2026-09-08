95e0aa03c3eb71522cd102725dd833c93beb410a
Ehsan Doroudian <ehsan14doroudian04@gmail.com>
2026-09-08 14:33:20 +0330

    feat: create initial BoardFlow window

---
+++ app/main.py
@@ -0,0 +1,24 @@
+import sys
+
+from PySide6.QtWidgets import QApplication, QMainWindow
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("BoardFlow")
+        self.resize(800, 500)
+
+
+def main():
+    app = QApplication(sys.argv)
+
+    window = MainWindow()
+    window.show()
+
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
