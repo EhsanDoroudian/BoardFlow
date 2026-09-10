7e0317eddfa913008f987452de706c25e30d6867
Ehsan Doroudian <ehsan14doroudian04@gmail.com>
2026-09-10 03:57:25 +0330

    feat: sync clipboard history with ui

--- main.py
+++ main.py
@@ -3,26 +3,32 @@ import sys
 from PySide6.QtWidgets import (
     QApplication,
     QLabel,
+    QListWidget,
     QLineEdit,
     QMainWindow,
     QVBoxLayout,
     QWidget,
-    QPushButton,
 )
 
-from app.ui.styles import DARK_THEME, LIGHT_THEME
-
+from app.database.database import Database
+from app.ui.styles import DARK_THEME
+from app.clipboard.monitor import ClipboardMonitor
 
 class MainWindow(QMainWindow):
-    def __init__(self):
+    def __init__(self, database):
         super().__init__()
+
+        self.database = database
+        self.monitor = ClipboardMonitor()
 
         self.setWindowTitle("BoardFlow")
         self.resize(800, 500)
 
-        self.dark_mode = True
-
         self.setup_ui()
+        self.load_history()
+
+        self.monitor.text_copied.connect(self.handle_new_clipboard)
+        self.monitor.start()
 
     def setup_ui(self):
         central_widget = QWidget()
@@ -37,33 +43,40 @@ class MainWindow(QMainWindow):
         history_title = QLabel("Clipboard History")
         history_title.setObjectName("history_title")
 
-        theme_button = QPushButton("☀")
-        theme_button.setFixedSize(40, 40)
-        theme_button.clicked.connect(self.toggle_theme)
+        history_list = QListWidget()
+        history_list.setObjectName("history_list")
 
-        layout.addWidget(theme_button)
         layout.addWidget(title)
         layout.addWidget(search)
         layout.addWidget(history_title)
-        layout.addStretch()
+        layout.addWidget(history_list)
+
+        self.history_list = history_list
 
         self.setCentralWidget(central_widget)
 
-    def toggle_theme(self):
-        self.dark_mode = not self.dark_mode
+    def load_history(self):
+        items = self.database.get_clipboard_items()
 
-        if self.dark_mode:
-            self.setStyleSheet(DARK_THEME)
-        else:
-            self.setStyleSheet(LIGHT_THEME)
+        for item in items:
+            _, content, created_at = item
 
+            self.history_list.addItem(
+                f"{content}\n{created_at}"
+            )
+    def handle_new_clipboard(self, text):
+        self.database.add_clipboard_item(text)
+
+        self.history_list.insertItem(0, text)
 
 def main():
     app = QApplication(sys.argv)
 
     app.setStyleSheet(DARK_THEME)
 
-    window = MainWindow()
+    database = Database()
+
+    window = MainWindow(database)
     window.show()
 
     sys.exit(app.exec())
